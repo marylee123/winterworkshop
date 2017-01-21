e67e6a5049ee1c1a6eb2595cddd43bd65f0f8d76
marylee123 <ML@Marys-MacBook-Air.local>
2017-01-21 00:35:46 -0500

    harry potter
    harry potter info
    syntax errors, etc.

---
+++ Harry_Potter.rb
@@ -0,0 +1,91 @@
+Harry_Potter = { 
+	"author" => "J.K. Rowling",
+	"publisher" => "Bloomsbury"
+	"important characters" => ["Harry Potter", "Ron Weasley", "Hermione Granger", "Lord Voldermort", "Albus Dumbledore", "Severus Snape", "Rubeus Hagrid", "Draco Malfoy"]
+	"Voldermort's wand" => "13½in. long made of yew with a phoenix core."
+	"Harry's wand" => "11in. long, made of holly, and possessed a phoenix feather core."
+	"Ron's first wand" => "12in. long, made of ash, and had a core of unicorn hair that stuck out at the end of the wand. It originally belonged to his older brother Charlie."
+	"Ron's second wand" => "14in. long, made of willow, and had a core of unicorn hair."
+	"Hermione's wand" => "10¾in. long, made of vine wood, and possessed a dragon heartstring core."
+	"books" => [
+		{
+			"title" => "Harry Potter and the Philosopher's Stone",
+			"number of pages" => 233,
+			"release date" => {
+				"day" => 26,
+				"month" => "June",
+				"year" => 1997
+			}
+			"characters" => "Professer Quirrell"
+			"spells" => ["Winguardian Leviosa", "Alohomora"]
+			"objects" => ["Philosopher's Stone", "Invisiblity Cloak", "Mirror of Erised", "Wizard Chess", "Nimbus 2000"]
+			"places" => ["Kings Cross Platform 9 3/4", "The Guarded Place", "Diagon Alley", "Gringotts", "Ollivander Wands"]
+			"events" => ["The First First year student to play Quiddith in a while", "Detention"]
+			"THe Dead" => ["Lily Potter", "James Potter", "Professer Quirrell"]
+		} 
+		{
+			"title" =>"Harry Potter and the Chamber of Secrets",
+			"number of pages" => 251,
+			"release date" => {
+				"day" => 2,
+				"month" => "July",
+				"year" => 1998
+			} 
+			"good characters" => ["Dobby", "Fawkes", "Gilderoy Lockhart", "Moaning Myrtle", "Basilisk", "Colin Creevey", "Justin Finch-Fletchley", "Penelope", "Ginny", "Tom Riddle"]
+			"spells" => ["Lumos"]
+			"objects" => ["Flying car", "Sword", "Tom Riddle's Diary"]
+			"places" => ["Chamber of Secrets"]
+			"events" => ["Duelling Club", "ParselTougue"]
+			"The Injured" => ["Harry Potter", "Hermione Granger", "Colin Creevey", "Justin Finch-Fletchley", "Penelope", "Ginny"]
+		} 
+		{
+			"title" =>"Harry Potter and the Prison of Azkaban",
+			"number of pages" => 317,
+			"release date" => {
+				"day" => 8,
+				"month" => "July",
+				"year" => 1999
+			}
+			"good characters" => ["Buckbeak", "centaurs", "Sirius Black", "Remus Lupin"]
+			"bad characters" => ["Dementors", , "Peter Pettigrew"]
+			"spells" => ["Expecto Patronum"]
+			"objects" => ["Time Necklace"]
+			"places" => ["Prison of Azkaban", "Hogsmaede"]
+			"events" => ["Turning Time", "Learning the Truth", "Saving Buckbeak", "Saving Sirius Black"]
+			"The Dead/Injured" => ["Buckbeak", "Sirius Black"]
+		}
+		{
+			"title" =>"Harry Potter and the Goblet of Fire",
+			"number of pages" => 636,
+			"release date" => {
+				"day" => 8,
+				"month" => "July",
+				"year" => 2000
+			} 
+			"characters" => ["Viktor Krum", "Cedric Diggory", "Fleur Delacour", "Mad-Eye Moody", "Bartemius Crouch", "Bartemius Crouch Junior", "Rita Skeeter", "mermaids", "dragons"]
+			"bad characters" => ["Bartemius Crouch Junior", "Peter Pettigrew"]
+			"spells" => ["Accio", "Stupefy", "Cruciatus Curse", "Imperius Curse", "Avada Kadavra", "Expelliarmus", "Priori Incantatem"]
+			"objects" => ["Portkey", "The Goblet of Fire", "Firebolt"]
+			"places" => ["The Maze"]
+			"events" => ["Quidditch World Cup", "Triwizard Tournament", "Yule Ball"]
+			"The Dead" => ["Frank", "Cedric Diggory"]
+			
+		} 
+		{
+			"title" =>"Harry Potter and the Order of the Pheonix",
+			"number of pages" => 766,
+			"release date" => {
+				"day" => 21,
+				"month" => "June",
+				"year" => 2003
+			}
+			"good characters" => ["Mad-Eye Moddy", "Nymphadora Tonks", "Kingsley Shacklebolt", "Rufus Scrimgeour", "Arthur Weasley", "Sirius Black", "Aurors", "Luna Lovegood", "Rita Skeeter","Thestrals"]
+			"bad characters" => ["Death Eaters", "Bellatrix Lestrange", "Lucius Malfoy", "Professer Umbridge"]
+			"spells" => ["Expellinarmus", "Stupefy", "Expecto Patronum", "Accio"]
+			"objects" => ["The Prophecy"]
+			"places" => ["Hog's Head", "The Room of Requirment"]
+			"events" => ["Harry's Hearing", "Dumbledore's Army", "O.W.Ls", "Occlumency lessons", "Arthur's Injury", "Voldermort invades Harry's body"]
+			"The Dead" => ["Sirius Black"]
+		}
+	]
+}
